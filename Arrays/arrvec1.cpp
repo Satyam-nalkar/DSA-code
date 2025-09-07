9d7490ddeef990d0cd0c3031ebb8bf3e2e7bf9c3
Your Name <your.email@example.com>
2025-09-07 11:16:39 +0530

    array modified code

--- Arrays/arrvec1.cpp
+++ Arrays/arrvec1.cpp
@@ -18,6 +18,7 @@ int main(){
   vec.push_back(25);
   vec.push_back(35);
   vec.push_back(45);
+  vec.push_back(55);
   cout << "after push back size = " << vec.size() << endl;
   vec.pop_back();
     // cout << val << endl;
--- Arrays/arrvec2.cpp
+++ Arrays/arrvec2.cpp
@@ -9,6 +9,7 @@ int main(){
     vec.push_back(2);
     vec.push_back(3);
     vec.push_back(4);
+    vec.push_back(5);
   cout << vec.size() << endl;
   cout << vec.capacity() << endl; 
     return 0;
