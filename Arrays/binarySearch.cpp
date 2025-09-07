d8ef46283384c37f38acbe3f80f4b8ed5cc565da
Your Name <your.email@example.com>
2025-09-07 11:17:58 +0530

    binary search algorithm

--- Arrays/binarySearch.cpp
+++ Arrays/binarySearch.cpp
@@ -11,10 +11,12 @@ int binarySearch(vector<int> arr ,int tar){  // iterative
  
         if(tar > arr[mid]){
             st = mid + 1;
+
         }
         else if(tar < arr[mid]){
             end = mid -1;
         }
+        
         else {
             return mid;
         }
