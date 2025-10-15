023605b07194c7d89efedd249916312225df909d
Your Name <your.email@example.com>
2025-10-15 15:28:12 +0530

    array example

--- Arrays/ex.cpp
+++ Arrays/ex.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // Declare and initialize an array of integers
-    int numbers[7] = {10, 20, 30, 40, 50, 60, 70};
+    int numbers[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
     // Access and display array elements
     cout << "Array elements: ";
@@ -12,5 +12,6 @@ int main() {
     }
 
     cout << "\nThird element: " << numbers[2]; // Access by index
+    cout << "\nsixth element: " << numbers[5]; // Access by index
     return 0;
 }
