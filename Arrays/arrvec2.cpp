4bcb2c8f2649574384b7a41aee4c33f7aae3eb99
Your Name <your.email@example.com>
2025-10-22 21:58:50 +0530

    array vector code

--- Arrays/arrvec2.cpp
+++ Arrays/arrvec2.cpp
@@ -10,6 +10,8 @@ int main(){
     vec.push_back(3);
     vec.push_back(4);
     vec.push_back(5);
+    vec.push_back(6);
+    vec.push_back(7);
   cout << vec.size() << endl;
   cout << vec.capacity() << endl; 
     return 0;
