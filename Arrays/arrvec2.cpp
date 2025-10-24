7d617802a91e882453fd220338fc89acf4384f3b
Your Name <your.email@example.com>
2025-10-24 11:45:31 +0530

    vector

--- Arrays/arrvec2.cpp
+++ Arrays/arrvec2.cpp
@@ -12,6 +12,7 @@ int main(){
     vec.push_back(5);
     vec.push_back(6);
     vec.push_back(7);
+    vec.push_back(8);
   cout << vec.size() << endl;
   cout << vec.capacity() << endl; 
     return 0;
