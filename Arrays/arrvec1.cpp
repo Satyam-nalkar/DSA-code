944fad2bac61bb42860738c7bfc0508aea2cdd55
Your Name <your.email@example.com>
2025-11-02 10:05:33 +0530

    push_back in vector code1

--- Arrays/arrvec1.cpp
+++ Arrays/arrvec1.cpp
@@ -24,6 +24,7 @@ int main(){
   vec.push_back(75);
   vec.push_back(85);
   vec.push_back(95);
+  vec.push_back(105);
   cout << "after push back size = " << vec.size() << endl;
   vec.pop_back();
     // cout << val << endl;
