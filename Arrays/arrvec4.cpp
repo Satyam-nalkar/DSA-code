ed40f3cd4436cd2285a3fec744742bbe895d44e4
Your Name <your.email@example.com>
2025-11-08 09:26:30 +0530

    vector program

--- Arrays/arrvec4.cpp
+++ Arrays/arrvec4.cpp
@@ -9,6 +9,7 @@ int main()
     // Accessing using operator[]
     cout << "Element at index 2 using []: " << v[2] << endl;
     cout << "Element at index 3 using []: " << v[3] << endl;
+    cout << "Element at index 1 using []: " << v[1] << endl;
     
     // Accessing using at()
     cout << "Element at index 3 using at(): " << v.at(3) << endl;
