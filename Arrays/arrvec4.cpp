7dcd33e9ed1484bd9b3e671219a8ef5d146c9c12
Your Name <your.email@example.com>
2025-11-24 09:51:23 +0530

    vector program

--- Arrays/arrvec4.cpp
+++ Arrays/arrvec4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    vector<int> v = {10, 20, 30, 40,50,60,1};
+    vector<int> v = {10, 20, 30, 40,50,60,1,2,3};
     
     // Accessing using operator[]
     cout << "Element at index 2 using []: " << v[2] << endl;
@@ -12,6 +12,8 @@ int main()
     cout << "Element at index 1 using []: " << v[1] << endl;
     cout << "Element at index 4 using []: " << v[4] << endl;
     cout << "Element at index 6 using []: " << v[6] << endl;
+    cout << "Element at index 7 using []: " << v[7] << endl;
+    cout << "Element at index 8 using []: " << v[8] << endl;
     
     // Accessing using at()
     cout << "Element at index 3 using at(): " << v.at(3) << endl;
