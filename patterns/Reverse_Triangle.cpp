499cb0aff1000b5f26b37ced23d96e79ab84d53d
Your Name <your.email@example.com>
2025-12-24 14:02:54 +0530

    hollow diamond pattern

--- patterns/Reverse_Triangle.cpp
+++ patterns/Reverse_Triangle.cpp
@@ -49,7 +49,7 @@ int main(){
 
 
 // pyramid pattern
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 int n = 4;
 
@@ -67,5 +67,48 @@ int main(){
         
         cout << endl;
     }
-    
-}
+}*/
+
+
+
+#include<iostream>
+using namespace std;
+int n = 4;
+
+int main(){
+    // up part
+    for (int i = 0; i < n; i++){
+        for(int j = 0;j<n-i-1; j++){
+            cout <<" ";
+        }
+        cout << "*";
+
+        if(i != 0){
+            for(int j =0; j< 2*i -1; j++){
+                cout << " ";
+            }
+
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // bottom part
+    for(int i = 0; i < n-1; i++){
+    //    space
+        for(int j =0; j < i+1; j++){
+            cout << " ";
+        }
+        cout << "*";
+        if(i != n-2){
+            // space
+           for(int j = 0; j<2*(n-i)-5; j++){
+            cout << " ";
+           }
+           cout << "*";
+        }
+        cout << endl;
+
+    }
+    return 0;
+    }
