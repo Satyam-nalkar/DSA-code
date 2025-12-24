ce04b92f1a29dbf9972b798ed8527324d72afd58
Your Name <your.email@example.com>
2025-12-24 09:41:50 +0530

    floyd's Triangle Pattern

--- patterns/Reverse_Triangle.cpp
+++ patterns/Reverse_Triangle.cpp
@@ -11,3 +11,19 @@ int main(){
     }
 }*/
 
+
+//floyd's Triangle Pattern
+#include<iostream>
+using namespace std;
+
+int n = 4;
+int num = 1;
+int main(){
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j < i+1 ; j++){
+            cout<< num << " ";
+            num ++ ;
+        }
+        cout << endl;
+    }
+}
