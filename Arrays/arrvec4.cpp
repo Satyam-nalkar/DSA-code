9c30c27e68b140ee23d73d3821199d9dde36ba02
Your Name <your.email@example.com>
2026-01-10 17:30:52 +0530

    element at 54 position

--- Arrays/arrvec4.cpp
+++ Arrays/arrvec4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    vector<int> v = {10, 20, 30, 40,50,60,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46};
+    vector<int> v = {10, 20, 30, 40,50,60,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47};
        
     // Accessing using operator[]
     cout << "Element at index 2 using []: " << v[2] << endl;
@@ -60,6 +60,7 @@ int main()
     cout << "Element at index 51 using []: " << v[51] << endl;
     cout << "Element at index 52 using []: " << v[52] << endl;
     cout << "Element at index 53 using []: " << v[53] << endl;
+    cout << "Element at index 54 using []: " << v[54] << endl;
     // Accessing using at()
     cout << "Element at index 3 using at(): " << v.at(3) << endl;
     cout << "Element at index 1 using at(): " << v.at(1) << endl;
